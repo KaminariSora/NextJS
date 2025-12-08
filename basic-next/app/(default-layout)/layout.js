a91b16c299db3ff087b3b5ad61c4bac291fea7a7
KaminariSora <nonthacha.h@gmail.com>
2025-12-08 23:16:57 +0700

    Update page layout

--- basic-next/app/(default-layout)/layout.js
+++ basic-next/app/(default-layout)/layout.js
@@ -1,13 +1,17 @@
 import Header from "../components/ui/header";
 import Footer from "../components/ui/footer";
+import ChatIcon from "../components/icons/chatIcon";
 
 export default function DefaultLayout({ children }) {
   return (
     <>
-      <Header/>
+      <Header />
       <main>
         {children}
       </main>
+      <a href="/chat" className="chatbot-btn">
+        <ChatIcon />
+      </a>
       <Footer />
     </>
   );
